3722cb56a3fc2bad6d2e5ecbe7c4542998d1e268
Francesco Ghizzo <francescoghizzo@MacBook-Pro-di-Francesco.local>
2022-01-19 21:23:54 +0100

    cambiati i nomi alle variabili

--- Python/WebScraping.py
+++ Python/WebScraping.py
@@ -4,13 +4,12 @@ from bs4 import BeautifulSoup
 import pandas as pd
 
 url = "https://www.w3schools.com/html/html_tables.asp"	
-page = requests.get(url)					# get page from a given url
+response = requests.get(url)					# get page from a given url
 
-encoding = page.encoding
-decode = page.content.decode(encoding)				# decode page content in non human readable form 
+encoding = response.encoding
+page = response.content.decode(encoding)			# decode page content in non human readable form 
 
-soup = BeautifulSoup(page.text, 'lxml')				# format page in a human readable form
+formatted_page = BeautifulSoup(response.text, 'lxml')		# format page in a human readable form
 
-table = pd.read_html(decode, attrs = {'id': 'customers'})	# read table from html using pandas. Select table using id
+table = pd.read_html(page, attrs = {'id': 'customers'})		# read table from html using pandas. Select table using id
 table = table[0]						# table is a list. Turn table into a pandas DataFrame
-
