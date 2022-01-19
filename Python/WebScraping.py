f9d80f968f983392d90003c66391a462f885f96a
Francesco Ghizzo <francescoghizzo@MacBook-Pro-di-Francesco.local>
2022-01-19 21:04:56 +0100

    Aggiunto WebScraping.py

---
+++ Python/WebScraping.py
@@ -0,0 +1,15 @@
+import requests
+import lxml
+from bs4 import BeautifulSoup
+import pandas as pd
+
+url = "https://www.w3schools.com/html/html_tables.asp"	
+page = requests.get(url)					# get page from a given url
+
+encoding = page.encoding
+decode = page.content.decode(encoding)				# decode page content in non human readable form 
+
+soup = BeautifulSoup(page.text, 'lxml')				# format page in a human readable form
+
+table = pd.read_html(decode, attrs = {'id': 'customers'})	# read table from html using pandas. Select table using id
+
