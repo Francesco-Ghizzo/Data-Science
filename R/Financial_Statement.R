d86caf58ab90cdc4dc71b982a34b6f8f72c3421c
Francesco Ghizzo <francescoghizzo@MacBook-Pro-di-Francesco.local>
2022-01-20 15:38:35 +0100

    Ho iniziato a scrivere le prime linee della mia versione del codice

---
+++ R/Financial_Statement.R
@@ -0,0 +1,18 @@
+## install ##
+
+# install.packages("tidyverse")
+# install.packages("janitor")
+
+
+## library ##
+
+library(tidyverse)
+library(janitor)
+
+
+## wrangling ##
+
+data_frame = read.csv2("Financial_Statement.csv")							#	use "read.csv2" if sep = ";". If sep = "," use "read.csv" instead 
+col_names = janitor::make_clean_names(colnames(data_frame))
+
+  
