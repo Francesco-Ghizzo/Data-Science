0d2386a405894a30d736db515840ae0b2d69f57d
Francesco Ghizzo <francescoghizzo@MacBook-Pro-di-Francesco.local>
2022-01-24 09:44:32 +0100

    Aggiunta nuova linea con la funzione 'separate' per scompattare la prima colonna

--- R/Financial_Statement.R
+++ R/Financial_Statement.R
@@ -13,6 +13,5 @@ library(janitor)
 ## wrangling ##
 
 data_frame = read.csv2("Financial_Statement.csv")							#	use "read.csv2" if sep = ";". If sep = "," use "read.csv" instead 
-col_names = janitor::make_clean_names(colnames(data_frame))
-
-  
+# col_names = janitor::make_clean_names(colnames(data_frame))
+separate(data_frame, Código.da.Conta, into=c("Tier1", "Tier2", "Tier3", "Tier4", "Tier5"))
